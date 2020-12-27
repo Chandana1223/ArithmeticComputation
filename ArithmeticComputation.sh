7649990818f54decd1329c7149da66d6d92c402d
Chandana1223 <chandana.k232@gmail.com>
2020-12-27 14:08:52 +0530

    Adding ArithmeticComputation.sh

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 
 echo "Arithmetic Computation and Sorting"
+
 echo "Enter first number:"
 read a
 echo "Enter second number:"
 read b
 echo "Enter third number:"
 read c
+
+compute1=$(( $a + $b * $c ))
